342e187a3d6888907ecf4a966767547b20d741f6
ihValery <ihValery@gmail.com>
2021-07-26 21:07:12 +0300

    Initial Commit

---
+++ NovaPoshta/NovaPoshtaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovaPoshtaApp.swift
+//  NovaPoshta
+//
+//  Created by Валерий Игнатьев on 26.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct NovaPoshtaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

54ce9eb35ca6d46251fc8c5188bf3a7e88bd4516
ihValery <ihValery@gmail.com>
2021-07-27 21:12:24 +0300

    Финал

--- NovaPoshta/View/SmallTrapeze.swift
+++ NovaPoshta/View/SmallTrapeze.swift
@@ -37,18 +37,22 @@ struct SmallTrapeze: View {
                                 Text("Новости")
                             })
                             
-                            
-                            
+                            LineRed()
                             Button(action: {}, label: {
                                 Text("Центр поддержки")
                             })
 
+                            LineRed()
                             Button(action: {}, label: {
                                 Text("Документация")
                             })
 
+                            LineRed()
                         }
+                        .font(.title3)
+                        .foregroundColor(.white)
                         
+                        Spacer()
                 })
         }
         .offset(y: showAbout ? 0 : 200)
@@ -58,5 +62,15 @@ struct SmallTrapeze: View {
 struct SmallTrapeze_Previews: PreviewProvider {
     static var previews: some View {
         SmallTrapeze(showAbout: .constant(true))
+    }
+}
+
+struct LineRed: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(LinearGradient(gradient: Gradient(colors: [.redApp, .white.opacity(0.3), .redApp]), startPoint: .leading, endPoint: .trailing))
+                .frame(width: UIScreen.main.bounds.width * 2 / 3, height: 1)
+        }
     }
 }
